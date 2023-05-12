93ce8052d8eafc772511fefd92eee9c784667dcd
Evgeny Eroshin <eee333@yandex.ru>
2023-05-13 02:05:03 +0300

    стили поправил

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -38,9 +38,8 @@ function ToDoItem({title, isComlete, changeStatus, id, index,
                 disabled={curMode == 'edit'}
             />)}
             {currPage == -1 && curMode != 'edit' &&
-            
-                <svg  class="btn-add-edit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="right-arrow" fill="currentColor"><path d="M22.707,12.707a1,1,0,0,0,0-1.414l-6-6a1,1,0,0,0-1.414,1.414L19.586,11H2a1,1,0,0,0,0,2H19.586l-4.293,4.293a1,1,0,0,0,1.414,1.414Z"></path></svg>
-            
+                // <svg  class="btn-add-edit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="right-arrow" fill="currentColor"><path d="M22.707,12.707a1,1,0,0,0,0-1.414l-6-6a1,1,0,0,0-1.414,1.414L19.586,11H2a1,1,0,0,0,0,2H19.586l-4.293,4.293a1,1,0,0,0,1.414,1.414Z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="14" id="arrow"><path fill="none" fill-rule="evenodd" stroke="#333" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.6" d="M1 7h16M11 1l6 6-6 6"></path></svg>
             }
         </li>
     )
